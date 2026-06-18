a21bc296fdedf585201266377f856c27dc6f168f
Matias <matiasmayans@gmail.com>
2026-06-18 15:59:00 -0300

    Koch

--- kochplot.py
+++ kochplot.py
@@ -2,25 +2,31 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import math
 
-x, y = 0.0, 0.0
-angulo = math.radians(30)
+def compute_segments(iterations):
+    segments = []
+    x,y = 0.0, 0.0
+    angulo = math.radians(30)
+    for i in range(iterations):
+        if i.bit_count() % 2 == 0:
+            angulo += math.radians(120)
+        else:
+            x2 = x + 10 * math.cos(angulo)
+            y2 = y + 10 * math.sin(angulo)
+            segments.append([(x, y), (x2, y2)])
+            x, y = x2, y2
+    return segments
 
-segments = []
-total = 2000000
+def plot(segments):
+    fig, ax = plt.subplots(figsize=(8, 8), facecolor="black")
+    ax.set_facecolor("black")
+    ax.add_collection(LineCollection(segments, colors="white", linewidths=1))
+    ax.autoscale()
+    ax.set_aspect("equal")
+    ax.axis("off")
+    plt.show()
 
-for i in range(total):
-    if i.bit_count() % 2 == 0:
-        angulo += math.radians(120)
-    else:
-        x2 = x + 10 * math.cos(angulo)
-        y2 = y + 10 * math.sin(angulo)
-        segments.append([(x, y), (x2, y2)])
-        x, y = x2, y2
+def koch_plot(iterations):
+    segments = compute_segments(iterations)
+    plot(segments)
 
-fig, ax = plt.subplots(figsize=(8, 8), facecolor="black")
-ax.set_facecolor("black")
-ax.add_collection(LineCollection(segments, colors="white", linewidths=1))
-ax.autoscale()
-ax.set_aspect("equal")
-ax.axis("off")
-plt.show()
+koch_plot(20000)
